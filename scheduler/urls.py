362897c6c226d0145663f1b67f04617b746d1e6e
Koushik Nakka <venkatakoushik777@gmail.com>
2026-03-10 22:13:36 -0400

    fix: mount static serving explicitly in urls.py to bypass WhiteNoise MD5 hash missing errors

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,6 +4,8 @@ URL configuration for the scheduler project.
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.views.generic import TemplateView
 
@@ -12,4 +14,4 @@ urlpatterns = [
     path('api/', include('jobs.urls')),
     path('metrics/', include('metrics.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
